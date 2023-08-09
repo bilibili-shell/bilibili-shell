b9461ac813ca729b4e318fddb2ff523d4f9d44d5
xingkong23333 <1036397001@qq.com>
2023-08-09 13:21:38 +0800

    feat: println qrcode to the console

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod protocol;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
-    info!("Hello, world!");
+    info!("Please scan the qrcode on your phone to login bilibili."); 
+    let code = protocol::login::qrcode::QrCode::apply().await?;
+    code.println().await?;
     Ok(())
 }
--- src/protocol/login/qrcode.rs
+++ src/protocol/login/qrcode.rs
@@ -3,6 +3,8 @@ use crate::protocol::login::bean::qrcode_apply;
 
 use super::bean::qrcode_apply::QCARRS;
 
+use qrcode::QrCode as PureQrCode;
+
 pub type QrCode = qrcode_apply::QCARDS;
 impl QrCode {
     pub async fn apply() -> Result<QrCode, Box<dyn std::error::Error>> {
@@ -10,5 +12,11 @@ impl QrCode {
         let text = Request::new(&url).get_text().await?;
         let obj: QCARRS = serde_json::from_str(&text)?;
         Ok(obj.data)
+    }
+    pub async fn println(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let code = PureQrCode::new(&self.url)?;
+        let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
+        println!("{}", string);
+        Ok(())
     }
 }
